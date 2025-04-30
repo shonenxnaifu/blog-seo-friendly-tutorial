cacd2b69083f5755b23ea1efec9226eed3ff263f
Pawitra Warda <pawitrawarda@gmail.com>
2025-04-30 11:17:21 +0700

    update sql

---
+++ project files/sql_command.sql
@@ -0,0 +1,18 @@
+-- 1 grant user permission
+grant create on schema public to anon
+
+-- 2 create increment function
+create or replace function increment (slug_text text) returns void AS $$
+declare view_id int;
+begin
+
+select id into view_id from views where slug = slug_text limit 1;
+
+if view_id is not null then
+update views set count = count + 1 where id = view_id;
+else
+insert into views (slug, count) values (slug_text, 1);
+end if;
+end;
+$$ language plpgsql
+
